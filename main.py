94bc4e7ac9833fa8037c9132cbed81affda215b5
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-21 12:33:41 -0300

    feat: call consume method in main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from rabbitmq import RabbitMQ
 import sys
 
 
+def callback(ch, method, properties, body):
+    print(f"Received message: {body}")
+
+
 def main():
     rabbitmq = RabbitMQ()
     try:
         rabbitmq.connect()
         print("Connection to RabbitMQ established successfully.")
+
+        rabbitmq.consume(queue_name="test_queue", callback=callback)
     except Exception as e:
         print(f"Failed to establish connection to RabbitMQ: {e}")
         sys.exit(1)
