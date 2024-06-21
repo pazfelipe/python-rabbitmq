6cdac8ef45151ecd198e66ea78286cc646fee5aa
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-21 12:39:28 -0300

    refactor: update main.py to use refactored RabbitMQ class

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def callback(ch, method, properties, body):
 def main():
     rabbitmq = RabbitMQ()
     try:
-        rabbitmq.connect()
         print("Connection to RabbitMQ established successfully.")
-
         rabbitmq.consume(queue_name="test_queue", callback=callback)
     except Exception as e:
         print(f"Failed to establish connection to RabbitMQ: {e}")
         sys.exit(1)
+    finally:
+        rabbitmq.close()
 
 
 if __name__ == "__main__":
